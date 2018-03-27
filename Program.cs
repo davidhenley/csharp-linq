ace2f2232fb22a7800323c0969046aab572b8e4f
David Henley <henleydr@gmail.com>
2018-03-27 16:42:41 -0500

    Init Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQ
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
